8138f9ab7334e57412ec36b4dbdab2d9e04dfab3
Aarush-Goyal <goyalaarush92@gmail.com>
2021-08-19 06:28:35 +0530

    updated: chat body

--- components/chat/ChatBody.tsx
+++ components/chat/ChatBody.tsx
@@ -1,6 +1,7 @@
 import { Flex, Box, Heading, Divider, Spacer, Input, Button, Image, Text } from '@chakra-ui/react'
 import React from 'react'
 import ChatProvider from '../../store/providers/ChatProvider'
+import ChatHeader from './ChatHeader'
 import OtherPersonChat from './OtherPersonChat'
 import PersonalChat from './PersonalChat'
 
@@ -10,26 +11,8 @@ const ChatBody = () => {
         <ChatProvider>
             <Flex borderColor="gray.300" shadow="sm" h="85vh" borderWidth="thin" p="4" my="10" overflow="hidden" rounded="md" flexDir="column">
 
-                <Flex w="100%" mt="2" flexDir="column" h="fit-content" p="6" borderWidth="thin" borderColor="gray.300" rounded="md" >
 
-                    <Flex mb="4">
-
-                        <Image src="https://facebookbrand.com/wp-content/uploads/2019/04/f_logo_RGB-Hex-Blue_512.png?w=512&h=512" objectFit="contain" w="10" />
-
-                        <Box ml="4">
-                            <Heading fontSize="2xl" >Facebook</Heading>
-
-                            <Text color="gray.600" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, porro!</Text>
-                        </Box>
-                    </Flex>
-                    <Divider />
-                    <Flex align="center" mt="4">
-                        <Heading fontSize="lg" color="gray.700">Lorem ipsum dolor sit amet consectetur adipisicing.</Heading> <Spacer />
-                        <Text color="green.500" >$ 113K - 120k</Text>
-                    </Flex>
-
-                </Flex>
-
+                <ChatHeader />
 
 
                 <Flex p="4" my="4" overflowY="scroll" minH="55vh" maxHeight="full" overscrollY="auto" flexDir="column" rounded="md">
